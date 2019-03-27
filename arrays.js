9792ab890c592bf9e9ba047b50bbceb974e436f0
Joe Jamsky <cba1067950@yahoo.com>
2019-03-27 01:03:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snikers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  array = ["element", ...array];
+  array = [element, ...array];
   return array;
 }
 
